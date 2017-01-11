4f8e2ec44186bf265cd9d03dc8ec0496beb83c47
Dan Anstis <dan@bsod.co.nz>
2017-01-11 16:15:17 +1000

    Update URL format regex

--- automatic/dattodrive/update.ps1
+++ automatic/dattodrive/update.ps1
@@ -4,7 +4,7 @@ import-module au
 
 function global:au_GetLatest{
     $downloadPage = Invoke-WebRequest -Uri 'https://download.dattodrive.com/'
-    $version = $downloadPage.Content | Select-String -Pattern "href=`"/win/dattodrive-([\d\.]*?)-.*?exe" | %{$_.Matches.Groups[1].Value}
+    $version = $downloadPage.Content | Select-String -Pattern "/dattodrive-([\d\.]*?)-.*?exe" | %{$_.Matches.Groups[1].Value}
     $downloadUrl = "https://download.dattodrive.com" + ($downloadPage.Content | Select-String -Pattern "href=`"(/win/.*?)`".*?Windows" | %{$_.Matches.Groups[1].Value})
 
     return @{ URL32 = $downloadUrl; Version = $version }
